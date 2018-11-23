9e486c94cdff1287c75f7dc2daa6ebbf134d8eb9
hl <501250891@qq.com>
2018-11-23 21:08:49 +0800

    Test

---
+++ day03/src/main/java/com/baizhi/springboot/Day03Application.java
@@ -0,0 +1,12 @@
+package com.baizhi.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day03Application.class, args);
+    }
+}
